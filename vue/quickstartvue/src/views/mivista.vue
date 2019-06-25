b9b7115459dbac9bcea60703f2647a1c256f0b75
Roberto <roberto.cabrales@uabc.edu.mx>
2019-06-24 19:43:45 -0700

    vuex, vuetify, etc...

---
+++ vue/quickstartvue/src/views/mivista.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="alert alert-primary" role="alert">{{propiedadNueva}}</div>
+</template>
+
+<<script>
+import { mapState, mapActions, mapGetters } from "vuex";
+
+export default {
+    name: "mivista",
+    components: {},
+    data() {
+        return {
+        texto: "Mi Vista"
+        };
+    },
+    computed: {
+        ...mapState({
+            propiedadNueva: "propiedadNueva"
+        })
+    },
+    methods: {}
+};
+
+</script>
+
+<style scoped>
+<style>
